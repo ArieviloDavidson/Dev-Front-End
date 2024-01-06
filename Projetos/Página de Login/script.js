b1e64fc3291d41e8432a137d2d4119a86a43b945
Your Name~ <you@example.com>
2024-01-06 09:42:23 -0300

    Atualização pagina de login

---
+++ Projetos/Página de Login/script.js
@@ -0,0 +1,5 @@
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    alert('Nome de usuário cadastrado: ' + username + '\nSenha cadastrada: ' + password);
+};
