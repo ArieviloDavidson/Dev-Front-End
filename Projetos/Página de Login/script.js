345d5cea919e59fdc73caffa07b1ce8903fd7ccc
Your Name~ <you@example.com>
2024-05-17 13:22:17 -0300

    Recomeço

--- Projetos/Página de Login/script.js
+++
@@ -1,5 +0,0 @@
-function login() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    alert('Nome de usuário cadastrado: ' + username + '\nSenha cadastrada: ' + password);
-};
